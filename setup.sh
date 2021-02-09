86a4a3ea705fd30ec17ae81c3789f976772f1031
Peter Bullock <34286598+PGBRULES@users.noreply.github.com>
2021-02-09 12:19:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ yum update -y
 yum upgrade -y
 yum install net-tools -y
 yum install nano -y
-wget https://cdn.discordapp.com/attachments/706345346963341393/801899508064714802/openvpn-as-2.5-CentOS7.x86_64.rpm
-rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
+wget https://openvpn.net/downloads/openvpn-as-latest-CentOS8.x86_64.rpm
+rpm -i openvpn-as-latest-CentOS8.x86_64.rpm
 cd /usr/local/openvpn_as/lib/python2.7/site-packages/
 rm pyovpn-2.0-py2.7.egg
 wget https://cdn.discordapp.com/attachments/706345346963341393/801899522020081684/pyovpn-2.0-py2.7.egg
