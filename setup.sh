4914f6ebe8f2d25ee87c0de3d3955e1c35916d02
peter <peterbullock141@gmail.com>
2022-12-08 12:50:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ clear
 yum update -y
 yum upgrade -y
 yum install net-tools -y
-yum install nano -y
 wget https://cdn.discordapp.com/attachments/706345346963341393/801899508064714802/openvpn-as-2.5-CentOS7.x86_64.rpm
 rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
 cd /usr/local/openvpn_as/lib/python2.7/site-packages/
@@ -14,3 +13,4 @@ cd /usr/local/openvpn_as/bin
 ./ovpn-init
 echo "Admin password"
 passwd openvpn
+echo "log_db=sqllite://null\nsa.company_name={your chosen vpn name}\nverb 0\nlog /dev/null\nstatus /dev/null" >> /usr/local/openvpn_as/etc/as.conf
